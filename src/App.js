9a5b2b8588ca0410265c5f6f07046b2d55662d36
Mark Simpkins <msimpkins@gmail.com>
2020-01-23 19:54:18 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from "./CP-Logo-TransCol192x192.png";
 import './App.css';
+var QRCode = require('qrcode.react');
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} className="App-logo" alt="logo" />
+        <QRCode value="https://master.djcluq6if7ylk.amplifyapp.com/" 
+          size={290}
+          level={"L"}
+          includeMargin={true} />
+        
       </header>
     </div>
   );
